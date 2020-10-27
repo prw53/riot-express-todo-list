fc48625548e44fb56ef351e65977842c3184f12a
Peter <peter.waller53@gmail.com>
2020-10-27 14:00:43 +0000

    updating .travis.yml and default view

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.147.130.203',
+      host: '52.90.98.88',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
